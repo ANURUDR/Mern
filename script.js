cc7b9a7e128ed20fdab03d2ab64c398f82f2fb51
Anurudr Saini <an40137747@wipro.com>
2025-02-10 12:06:27 +0530

    Your descriptive commit message

---
+++ script.js
@@ -0,0 +1,15 @@
+const user =
+{
+    name : "hitesh",
+    prices : 12
+
+}
+
+
+function my_fun (anyobj)
+{
+    console.log(`Username is ${anyobj.name} and price is ${anyobj.prices}` )
+}
+
+
+my_fun(user);
